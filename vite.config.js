2c9808702ec91de58d45cbfac410eece33204d05
AntonioTarasenko <toxata1@gmail.com>
2024-12-29 11:05:51 +0200

    поправка

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'copy-from-OualiCode-s-work',
+  base: '/copy-from-OualiCode-s-work/',
 })
